6328f54c84edfd8243d242431f43613144463867
Italo Franco <italo.ravina@gmail.com>
2020-03-24 20:35:01 +0100

    modified the api routes, i create route for search by ID ad

--- keepads/routes/api/ads.js
+++ keepads/routes/api/ads.js
@@ -11,10 +11,40 @@ const Advertisement = require("../../models/Advertisement");
  * Returns list of announcements
  */
 
-router.get("/", (req, res, next) => {
-    Advertisement.find().exec((err, ads) => {
+router.get("/", async (req, res, next) => {
+    
+    try{
+
+        const ads = await Advertisement.find();
         res.json(ads);
-    })
+    }catch(err){
+
+        next(err);
+    }
+   
 });
+
+
+router.get("/:id", async (req, res, next) => {
+
+    try{
+
+        const _id = req.params.id;
+        const ad = await Advertisement.findOne({ _id });
+
+        if(!ad){
+            const err = new Error("Ad Not found");
+            err.status = 404;
+            next(err);
+            return;
+        }
+        res.json(ad);
+    }catch(err){
+
+        next(err);
+    }
+
+});
+
 
 module.exports = router;
