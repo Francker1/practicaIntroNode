2dabbee236006a768ad2af9a9808e5d2a146569f
Italo Franco <italo.ravina@gmail.com>
2020-04-11 16:33:33 +0200

    last fix swagger and multer dest

--- keepads/routes/api/ads.js
+++ keepads/routes/api/ads.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const upload = multer({ dest: "../../public/images/ads/" })
+const upload = multer({ dest: "./public/images/ads/" })
 
 const helperJS = require("../../public/javascripts/helper");
 
@@ -157,7 +157,7 @@ router.post("/", upload.single("photo"), async (req, res, next) => {
  *               $ref: '#/definitions/Advertisement'
  *      responses:
  *       200:
- *         description: Created!
+ *         description: Updated!
  */
 router.put("/:id", async (req, res, next) => {
 
--- keepads/routes/api/api-docs.js
+++ keepads/routes/api/api-docs.js
@@ -10,7 +10,6 @@ const swaggerUI = require("swagger-ui-express");
 //Swagger documentation
 const swaggerOptions = {
     swaggerDefinition:{
-        openapi: "3.0.0",
         info:{
             version: "1.0.0",
             title: "KeepCodingAds API",
